cf1b7ff58c1d074a1e36c890daedf5089ec1e35a
Princeodi <110994588+Princeodi@users.noreply.github.com>
2023-06-28 21:35:33 +0100

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,14 +1,22 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve) => {
-  
+  return new Promise((resolve, reject) => {
+    // Simulating an asynchronous API call
     setTimeout(() => {
-     
-      const response = {
-        data: "Sample response data",
-        status: 200
-      };
+      // Simulating an error condition
+      const errorOccurred = true;
 
-      resolve(response);
-    }, 2000);
+      if (errorOccurred) {
+        // Reject the Promise with an error message
+        reject("An error occurred during the API call.");
+      } else {
+        // Assuming the API call is successful
+        const response = {
+          data: "Sample response data",
+          status: 200
+        };
+  
+        resolve(response); // Resolving the Promise with the response data
+      }
+    }, 2000); // Simulating a delay of 2000 milliseconds (2 seconds)
   });
 }
