71942435bc48c84298ffba803753918b09f275d6
Princeodi <110994588+Princeodi@users.noreply.github.com>
2023-06-28 22:58:52 +0100

    Update 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,15 +1,9 @@
 function handleResponseFromAPI(promise) {
-  promise
-    .then(response => {
-      console.log("Got a response from the API");
-      return {
-        status: 200,
-        body: "success"
-      };
-    })
-    .catch(error => {
-      console.error("An error occurred:", error);
-      return new Error();
-    });
+  const body = { status: 200, body: 'success' };
+
+  return promise
+    .then(() => body)
+    .catch((error) => error)
+    .finally(() => console.log('Got a response from the API'));
 }
-export default getFullResponseFromAPI;
+export default handleResponseFromAPI;
