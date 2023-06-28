1c682294e80538a5e700200471a2056b691fd8fb
Princeodi <110994588+Princeodi@users.noreply.github.com>
2023-06-28 23:39:42 +0100

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,19 +1,11 @@
 import { createUser, uploadPhoto } from './utils';
 
 function handleProfileSignup() {
-<<<<<<< HEAD
-	  return Promise.all([uploadPhoto(), createUser()])
-	    .then((data) => {
-		          console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
-		        })
-	    .catch(() => console.log('Signup system offline'));
-=======
   return Promise.all([uploadPhoto(), createUser()])
     .then((data) => {
       console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
->>>>>>> 24159c950a47dd7a45aa57e910b28dcee13a1349
 }
 
 export default handleProfileSignup;
