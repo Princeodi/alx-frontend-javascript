fed11b3cb2a74b641714752d3ec816f185baa6a9
Princewill Onyema <Princewillodinakachukwu@gmail.com>
2023-06-28 23:13:35 +0000

    final

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,5 +1,5 @@
-import signUpUser from "./4-user-promise.js";
-import uploadPhoto from "./5-photo-reject.js";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 async function handleProfileSignup(firstName, lastName, fileName) {
   const userPromise = signUpUser(firstName, lastName);
@@ -10,10 +10,10 @@ async function handleProfileSignup(firstName, lastName, fileName) {
 
     return [
       { status: userResponse.status, value: userResponse.value },
-      { status: photoResponse.status, value: photoResponse.value }
+      { status: photoResponse.status, value: photoResponse.value },
     ];
   } catch (error) {
-    console.error("An error occurred:", error);
+    console.error('An error occurred:', error);
     return [];
   }
 }
