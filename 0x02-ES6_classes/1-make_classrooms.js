358f3c9a1d4b85943a811606340a71054f224f84
Princeodi <110994588+Princeodi@users.noreply.github.com>
2023-06-29 15:33:54 +0100

    Update 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -2,7 +2,7 @@ import ClassRoom from './0-classroom';
 
 function initializeRooms() {
   const _maxStudentsSize = [19, 20, 34];
-  const rooms = _maxStudentsSize.map((_maxStudentsSize) => new ClassRoom(size));
+  const rooms = _maxStudentsSize.map((_maxStudentsSize) => new ClassRoom(_maxStudentsSize));
   return rooms;
 }
 
