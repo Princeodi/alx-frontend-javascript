3b8a3043ff1bd4a9236cae6117a55932f49c7a1a
Princeodi <110994588+Princeodi@users.noreply.github.com>
2023-06-29 19:16:23 +0100

    3-currency.js

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,32 +1,25 @@
-export default class Currency{
+export default class Currency {
   constructor(code, name) {
-    this._code = typeof code === 'string' ? code : '';
-    this._name = typeof name === 'string' ? name : '';
+    this._code = code;
+    this._name = name;
   }
 
   get code() {
     return this._code;
   }
 
-  set code(value) {
-    if (typeof value === 'string') {
-      this._code = value;
-    } else {
-      throw new TypeError('Code must be a string.');
-    }
-  }
-
   get name() {
     return this._name;
   }
 
-  set name(value) {
-    if (typeof value === 'string') {
-      this._name = value;
-    } else {
-      throw new TypeError('Name must be a string.');
-    }
+  set code(code) {
+    this._code = code;
   }
+
+  set name(name) {
+    this._name = name;
+  }
+
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
