79bafed2b734d9da462c5423b1a7624f58b89cd4
Princeodi <110994588+Princeodi@users.noreply.github.com>
2023-06-29 06:48:26 +0100

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,28 +1,8 @@
 function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-<<<<<<< HEAD
-    // Simulating an asynchronous API call
-    setTimeout(() => {
-      // Simulating an error condition
-      const errorOccurred = true;
-
-      if (errorOccurred) {
-        // Reject the Promise with an error message
-        reject('An error occurred during the API call.');
-      } else {
-        // Assuming the API call is successful
-        const response = {
-          data: 'Sample response data',
-          status: 200,
-        };
-
-        resolve(response); // Resolving the Promise with the response data
-      }
-    }, 2000); // Simulating a delay of 2000 milliseconds (2 seconds)
-=======
     if (success) resolve({ status: 200, body: 'Success' });
     reject(Error('The fake API is not working currently'));
->>>>>>> f5ed258ea80fbc5438fd4e745ba81f6bdfc849e2
   });
 }
+
 export default getFullResponseFromAPI;
